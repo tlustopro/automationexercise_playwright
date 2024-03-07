522bce1a29a9890c7903403237474303a855f557
Denis Aussenberg <d.aussenberg@gmail.com>
2024-03-06 22:14:46 -0600

    fix form test case

--- src/pages/contact.page.ts
+++ src/pages/contact.page.ts
@@ -5,15 +5,13 @@ import { faker } from '@faker-js/faker/locale/en';
 export class ContactPage{
     constructor(public page: Page){
     }
-    async FillUpForm() {
-      await this.page.waitForLoadState('networkidle') 
-      await this.page.getByTestId('name').fill(faker.person.fullName());
-      await this.page.getByTestId('email').fill(faker.internet.email()); 
-      await this.page.getByTestId('subject').fill(faker.word.words()); 
-      await this.page.getByTestId('message').fill(faker.word.words(20)); 
-      await this.page.locator('[name=upload_file]').setInputFiles('./files/dummy.pdf');
-      await this.page.getByTestId('submit-button').click();
-      }
 
-    
+    async FillUpForm() {
+        await this.page.getByTestId('name').fill(faker.person.fullName());
+        await this.page.getByTestId('email').fill(faker.internet.email()); 
+        await this.page.getByTestId('subject').fill(faker.word.words()); 
+        await this.page.getByTestId('message').fill(faker.word.words(20)); 
+        await this.page.locator('[name=upload_file]').setInputFiles('./files/dummy.pdf');
+        await this.page.locator('[name=submit]').click();
+    }
 }
--- src/tests/shopping.spec.ts
+++ src/tests/shopping.spec.ts
@@ -7,17 +7,17 @@ import { ContactPage } from '../pages/contact.page';
 
 test.describe('Shopping test cases', () => {
     test('TC_06_Contact_us_form', async ({ page }) => {
-    const homepage = new HomePage(page);
-    const contact = new ContactPage(page);
+        const contact = new ContactPage(page);
 
-    await homepage.OpenContactUsPage();
-    await contact.FillUpForm();
-    // accepting dialog using dialog handler TODO: FIX
-    page.on('dialog', dialog => {
-        dialog.accept();
-    }); 
-    await expect(page.locator("text=Success! Your details have been submitted successfully.")).toBeVisible()
-    await page.getByRole('button', { name: 'Home' }).click();
-    await expect(page).toHaveURL('/');
-    });
+        await page.goto('/contact_us');
+        page.on('dialog', async (dialog) => {
+          await dialog.accept();
+        });
+        await contact.FillUpForm();
+        await expect(
+          page.locator('#contact-page').getByText('Success! Your details have')
+        ).toBeVisible();
+        //TODO: FIX
+        await page.getByRole('link', { name: 'Home' }).click(); 
+      });
 });
